bffcd9f95c7077639572f3eaf868d81c5c152200
Kujin Lee <kujinlee@gmail.com>
2025-04-09 14:35:45 -0700

    chore: add backend base URL to test environment setup

--- tests/setupTests.js
+++ tests/setupTests.js
@@ -1,6 +1,8 @@
 process.env.NODE_ENV = 'test';
+process.env.BACKEND_APP_BASE_URL = '/flightreservation-node-es6-react-backend'; // Add backend base URL
 
 // Log the current environment
 console.log('Current NODE_ENV:', process.env.NODE_ENV);
+console.log('Current BACKEND_APP_BASE_URL:', process.env.BACKEND_APP_BASE_URL);
 
 // This file can be used to set up Jest-specific configurations or mocks.
